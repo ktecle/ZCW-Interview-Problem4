d611ae9de012264cbedd1bca53f241267fb3e319
Kibret Tecle <kibrettecle@zipcoders-MacBook-Pro-5.local>
2018-04-15 11:29:53 -0400

    Done

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,30 @@
 package io.zipcoder;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem4 {
+
+    public boolean isPalindromePermutation(String input) {
+        int oddOccurences = 0;
+        Map<Character, Integer> myChar = new HashMap<Character, Integer>();
+        for (int i = 0; i < input.length(); i++) {
+            if (!myChar.containsKey(input.charAt(i))) {
+                myChar.put(input.charAt(i), 1);
+            } else {
+                myChar.put(input.charAt(i), myChar.get(input.charAt(i))+1);
+            }
+        }
+        for (Character key : myChar.keySet()) {
+            if (myChar.get(key) % 2 != 0) {
+                oddOccurences++;
+            }
+        }
+
+        if (oddOccurences > 1) {
+            return false;
+        }
+        return true;
+
+    }
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,34 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem4Test {
+
+    @Test
+    public void testPalindromePermutation(){
+        Problem4 test = new Problem4();
+        String input = "aaabbbb";
+        boolean expected = true;
+        boolean actual = test.isPalindromePermutation(input);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testPalindromePermutation2(){
+        Problem4 test = new Problem4();
+        String input = "cdefghmnopqrstuvw";
+        boolean expected = false;
+        boolean actual = test.isPalindromePermutation(input);
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void testPalindromePermutation3(){
+        Problem4 test = new Problem4();
+        String input = "cdcdcdcdeeeef";
+        boolean expected = true;
+        boolean actual = test.isPalindromePermutation(input);
+        Assert.assertEquals(expected,actual);
+    }
 }
